d6a6679c3727d90e8b286a3ace40f3c18bfb9459
Ross Wilson <ross.wilson@homeflow.co.uk>
2024-05-23 20:12:07 +0100

    add unit test for core functions

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,51 @@ pub fn verify(
     s: &BigUint,
     p: &BigUint,
 ) -> bool {
-    let cond1 = *r1 == alpha.modpow(s, p) * y1.modpow(c, p);
-    let cond2 = *r2 == beta.modpow(s, p) * y2.modpow(c, p);
+    let cond1 = *r1 == (alpha.modpow(s, p) * y1.modpow(c, p)).modpow(&BigUint::from(1u32), p);
+    let cond2 = *r2 == (beta.modpow(s, p) * y2.modpow(c, p)).modpow(&BigUint::from(1u32), p);
     cond1 && cond2
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_toy_example() {
+        // TODO: split this into separate tests
+        let alpha = BigUint::from(4u32);
+        let beta = BigUint::from(9u32);
+        let p = BigUint::from(23u32);
+        let q = BigUint::from(11u32);
+
+        let x = BigUint::from(6u32);
+        let k = BigUint::from(7u32);
+
+        let c = BigUint::from(4u32);
+
+        let y1 = exponentiate(&alpha, &x, &p);
+        let y2 = exponentiate(&beta, &x, &p);
+
+        assert_eq!(y1, BigUint::from(2u32));
+        assert_eq!(y2, BigUint::from(3u32));
+
+        let r1 = exponentiate(&alpha, &k, &p);
+        let r2 = exponentiate(&beta, &k, &p);
+
+        assert_eq!(r1, BigUint::from(8u32));
+        assert_eq!(r2, BigUint::from(4u32));
+
+        let s = solve(&k, &c, &x, &q);
+        assert_eq!(s, BigUint::from(5u32));
+
+        let result = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s, &p);
+        assert!(result);
+
+        // fake secret
+        let x_fake = BigUint::from(7u32);
+        let s_fake = solve(&k, &c, &x_fake, &q);
+
+        let result = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s_fake, &p);
+        assert!(!result);
+    }
 }
